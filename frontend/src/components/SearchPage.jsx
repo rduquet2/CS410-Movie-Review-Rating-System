0dee24e77981009bc1c2423880a611243b43a177
richiez2 <116695365+richiez2@users.noreply.github.com>
2024-04-30 18:02:33 -0500

    Update SearchPage.jsx

--- frontend/src/components/SearchPage.jsx
+++ frontend/src/components/SearchPage.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
 import axios from 'axios'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 function SearchPage() {
-  const [movieTitle, setMovieTitle] = useState('')
+ const [movieTitle, setMovieTitle] = useState('')
+ const [recommendation, setRecommendation] = useState('')
+ const [percent, setPercent] = useState('')
 
-  const handleChange = (e) => {
-    setMovieTitle(e.target.value)
-  }
+ useEffect(() => {
+   axios.get('/getTitle')
+     .then(res => res.data())
+     .then(data => setRecommendation(data[0]))
+ })
 
-  const sendMovieTitle = (e) => {
-    e.preventDefault()
-    axios.post('/getTitle', {title: movieTitle})
-        .then(res => console.log(res.data))
-        .catch(error => console.error(error));
-  }
+ const handleChange = (e) => {
+   setMovieTitle(e.target.value)
+ }
 
-  return (
-    <div className='search-container' onSubmit={sendMovieTitle}>
-        <h1 className='search-title'>Search a Movie</h1>
-        <form className='search-form'>
-            <input type='text' onChange={handleChange}></input>
-            <button type='submit'>
-              <i className="fa-solid fa-arrow-right"></i>
-            </button>
-        </form>
-    </div>
-  )
+ const sendMovieTitle = (e) => {
+   e.preventDefault()
+   axios.post('/getTitle', {title: movieTitle})
+       .then(res => console.log('hi', res.data))
+       .catch(error => console.error(error));
+ }
+
+ return (
+   <div className='search-container' onSubmit={sendMovieTitle}>
+       <h1 className='search-title'>Search a Movie</h1>
+       <form className='search-form'>
+           <input type='text' onChange={handleChange}></input>
+           <button type='submit'>
+             <i className="fa-solid fa-arrow-right"></i>
+           </button>
+       </form>
+   </div>
+ )
 }
 
 export default SearchPage
